82c253daf00a03d0718e25701ac2c636eb3f06c9
Isuru Fernando <isuruf@gmail.com>
2026-02-03 13:20:05 -0800

    remove global options

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -eux
 
 if [[ "$use_noarch" == "true" ]]; then
-  PYYAML_FORCE_LIBYAML=0 ${PYTHON} -m pip install . --no-deps --no-build-isolation -vv
-else
-  ${PYTHON} -m pip install . --no-deps --no-build-isolation -vv --global-option="--with-libyaml" --global-option="build_ext" --global-option="-I${PREFIX}/include" --global-option="-L${PREFIX}/lib"
+  export PYYAML_FORCE_LIBYAML=0
 fi
+${PYTHON} -m pip install . --no-deps --no-build-isolation -vv
